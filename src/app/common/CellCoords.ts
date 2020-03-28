5591af14929c4220f12a7fbcc1b052b1b0db3b72
JFG <lapuff@yahoo.com>
2020-03-28 14:18:13 -0400

    This is the beginning

---
+++ src/app/common/CellCoords.ts
@@ -0,0 +1,5 @@
+export class CellCoords {
+    public RowPos = 0;
+    public ColPos = 0;
+    public valid = false;
+  }
---
+++ src/app/common/GameOption.ts
@@ -0,0 +1,6 @@
+export class GameOption {
+  public NbRow = 0;
+  public NbCol = 0;
+  public NbBomb = 0;
+  public MaxSec = 0;
+}
